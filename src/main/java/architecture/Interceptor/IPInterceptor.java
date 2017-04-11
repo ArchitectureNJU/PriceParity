2f3e4e1e71f5331e4efc25f2cb108509f9b919fa
陈天歌 <ctg14@software.nju.edu.cn>
2017-04-12 02:21:50 +0800

    ip检测、管理员查看禁止ip、添加禁止ip、添加禁止ip完成并测试 robot检测完成待测试 负载均衡待完成

--- src/main/java/architecture/Interceptor/IPInterceptor.java
+++ src/main/java/architecture/Interceptor/IPInterceptor.java
@@ -1,7 +1,26 @@
 package architecture.Interceptor;
 
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 /**
  * Created by chentiange on 2017/4/11.
  */
-public class IPInterceptor {
+public class IPInterceptor extends HandlerInterceptorAdapter {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String clientip = request.getRemoteAddr();
+        IPManager manager = new IPManager();
+        if (!manager.isIPVaild(clientip)){
+            //intercept this request
+            return false;
+        }
+        return true;
+    }
+
 }
--- src/main/java/architecture/listener/RobotListener.java
+++ src/main/java/architecture/listener/RobotListener.java
@@ -1,5 +1,6 @@
 package architecture.listener;
 
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -9,16 +10,18 @@ import java.util.Map;
 /**
  * Created by chentiange on 2017/4/12.
  */
-public class IPListener implements ServletContextListener {
+public class RobotListener implements ServletContextListener {
+
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         ServletContext context = servletContextEvent.getServletContext();
         //store ip
         Map<String, Long[]> ipmap = new HashMap<>();
         context.setAttribute("ipmap", ipmap);
-        //rejected ip
-        Map<String, Long> rejectedIpMap = new HashMap<String, Long>();
-        context.setAttribute("rejectedIpMap", rejectedIpMap);
+        //robot ip
+        Map<String, Long> robotIpMap = new HashMap<String, Long>();
+        context.setAttribute("robotIpMap", robotIpMap);
     }
 
     @Override
